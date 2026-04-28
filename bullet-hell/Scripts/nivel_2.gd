44cfd272cbe11b5f94e461fb821800f0cef3fca8
Albano <albanofazzito@gmail.com>
2026-04-27 23:15:10 -0300

    Se añadio muerte en la pantalla y se arreglo el boton de reinicio

--- bullet-hell/Scripts/nivel_2.gd
+++ bullet-hell/Scripts/nivel_2.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 var PteroEscena
 func _ready() -> void:
+	$Node2D2/CharacterBody2D.murio.connect(_on_raptor_murio)
 	PteroEscena=preload("res://Escenas/Pterodactilo.tscn")
 	$TimerPterodactilos.start()
 
@@ -11,6 +12,14 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func _on_raptor_murio():
+	var pos_pantalla= $Node2D2/CharacterBody2D.get_global_transform_with_canvas().origin
+	$Node2D2/CharacterBody2D/AnimatedSprite2D2.visible=true
+	$Node2D2/CharacterBody2D/AnimatedSprite2D2.play()
+	$Node2D2/CharacterBody2D/ColorRect.visible=true
+	$Node2D2/CharacterBody2D/AnimationPlayer.play("muerte")
+	$Node2D2/CharacterBody2D/ColorRect2.visible=true
+	get_tree().paused=true
 
 func _on_timer_pterodactilos_timeout() -> void:
 	var pterodactilo= PteroEscena.instantiate()
